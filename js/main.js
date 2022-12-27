acc97625e2d614786e2879f8fd37e05d091acd58
Ahmed Faysal <ahmedfoysal01797@gmail.com>
2022-12-28 02:10:00 +0600

    final check

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const navList = document.querySelector('.nav-list');
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('active')
-    navMenu.classList.toggle('active')
+    navList.classList.toggle('active')
 })
 
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () =>{
     hamburger.classList.remove('active')
-    navMenu.classList.remove('active')
+    navList.classList.remove('active')
 }))
